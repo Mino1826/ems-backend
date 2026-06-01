78ff3ace20167f2311e0ab7a886f073dbda1a70d
Mino Khosravi <mino.khosravi2025@gmail.com>
2026-06-01 18:08:09 +0330

    add findByLastName repository method

--- src/main/java/com/Mino/ems/repository/EmployeeRepository.java
+++ src/main/java/com/Mino/ems/repository/EmployeeRepository.java
@@ -3,12 +3,15 @@ package com.Mino.ems.repository;
 import com.Mino.ems.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     Optional<Employee> findByEmail(String email);
     boolean existsByEmail(String email);
+
+    List<Employee> findByLastName(String lastName);
 
 
 }
